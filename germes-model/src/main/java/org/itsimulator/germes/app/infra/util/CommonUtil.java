3dcd275daf9f25bc548303909c5d4ea1ddadefc1
victorandruscagit <victor.v.andrusca@gmail.com>
2021-09-26 20:43:33 +0300

    added getSafeList(List<T> source) in germes-model/CommonUtil

--- germes-model/src/main/java/org/itsimulator/germes/app/infra/util/CommonUtil.java
+++ germes-model/src/main/java/org/itsimulator/germes/app/infra/util/CommonUtil.java
@@ -1,6 +1,7 @@
 package org.itsimulator.germes.app.infra.util;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -21,5 +22,15 @@ public class CommonUtil {
 	public static <T> Set<T> getSafeSet(Set<T> source) {
 		return Collections.unmodifiableSet(Optional.ofNullable(source).orElse(Collections.emptySet()));
 	}
+	/**
+	 * Returns not-null unmodifiable copy of the source list
+	 * @param source
+	 * @return
+	 */
+	public static <T> List<T> getSafeList(List<T> source) {
+		return Collections.unmodifiableList(Optional.ofNullable(source).orElse(Collections.emptyList()));
+	}
 
 }
+
+
